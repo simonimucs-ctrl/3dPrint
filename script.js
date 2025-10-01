929e8709a4330dfbe1fa93808a05b96672f177e7
simonimucs-ctrl <simonimucs@gmail.com>
2025-10-01 15:56:45 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const costiBase = {
+  "modello_auto": 15.00, // Prezzo base per il Modello A
+  "modello_robot": 20.00, // Prezzo base per il Modello B
+  // ... altri modelli
+};
+
+const costiColore = {
+  "rosso": 1.50,
+  "nero": 2.00, // Il nero costa di più
+  "blu": 1.00,
+};
